4990a52ae37c51cd9f2d057a0fc9b45d534410c0
Vinicius Calebe Barbosa de Moura <111473558+vini010101@users.noreply.github.com>
2023-08-07 20:06:09 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,36 @@
+// alert("Hello World")
+// console.log("Só quem é dev vai descobir")
+// area de registro
+// VAR
+// LET
+// CONST
+// query=pesquisa, seletor
+let nome = document.querySelector("#nome")
+
+function exibirnome(){
+    console.log(nome.value)
+    if(nome.value == ""){
+        nome.style.border = "2px solid red"
+    }
+    else{
+        nome.style.border = "2px solid green"
+    }
+}
+
+nome.addEventListener("blur",exibirnome)
+// o addEventListener cria um evento para algum elemento, no caso estamos utilizando o evento "blur", que é qaundo o foco sai de um campo.
+
+// nome.addEventListener("focus", function(){
+//     nome.value = "Este campo não deve ficar vazio"
+// })
+let idade = document.querySelector("#idade")
+
+idade.addEventListener("blur",function(){
+    if(idade >= 18){
+        alert("voce possui mais de 18 anos!")
+    }
+    else
+    alert("infelizmente, voce nao é de maior!")
+    window.location.href = "https://www.youtube.com/watch?v=9a2YNYRsKus&pp=ygUPcmF0byBkYW7Dp2FuZG8g"
+})
+let endereco = document.querySelector()
