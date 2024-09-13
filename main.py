442ba85a8708b3591cc968e9b93c5d9355cb80e0
Vinicius Calebe Barbosa de Moura <111473558+vini010101@users.noreply.github.com>
2024-09-13 12:38:06 -0300

    mudei as cores, os textos e as fotos

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request, Form
+from fastapi import FastAPI, Request, Form  # Certifique-se de importar Request aqui
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -9,8 +9,9 @@ app = FastAPI()
 # Monta arquivos estáticos
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-# Configura o diretório de templates para a pasta "pages"
-templates = Jinja2Templates(directory="pages")
+# Configura os diretórios de templates
+templates_pages = Jinja2Templates(directory="pages")  # Diretório para o 'trabalhe_conosco.html'
+templates_index = Jinja2Templates(directory="pages")  # Diretório para o 'index.html'
 
 class FormData(BaseModel):
     nome: str
@@ -22,10 +23,15 @@ class FormData(BaseModel):
     trabalho: str
     mensagem: str
 
-# Renderiza o template HTML correto
+# Rota para a página inicial (index.html)
 @app.get("/", response_class=HTMLResponse)
-def read_form(request: Request):
-    return templates.TemplateResponse("trabalhe_conosco.html", {"request": request})
+def read_index(request: Request):  # Certifique-se de que "request" está presente como parâmetro
+    return templates_index.TemplateResponse("index.html", {"request": request})
+
+# Rota para o formulário (trabalhe_conosco.html)
+@app.get("/formulario", response_class=HTMLResponse)
+def read_form(request: Request):  # Certifique-se de que "request" está presente como parâmetro
+    return templates_pages.TemplateResponse("trabalhe_conosco.html", {"request": request})
 
 # Submissão do formulário
 @app.post("/submit")
